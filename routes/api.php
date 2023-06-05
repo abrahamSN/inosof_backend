f3accfc4e3ed10b2f01d3435157d846b73a1d89c
abrahamSN <abrahamsn.mayer@gmail.com>
2023-06-05 22:01:03 +0700

    init route api jwt

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('login', 'App\Http\Controllers\Api\AuthController@login');
+    Route::post('register', 'App\Http\Controllers\Api\AuthController@register');
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::get('logout', 'App\Http\Controllers\Api\AuthController@logout');
+        Route::get('user', 'App\Http\Controllers\Api\AuthController@user');
+    });
+});
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('vehicle')->group(function () {
+        Route::get('/', 'App\Http\Controllers\Api\VehicleController@index');
+        Route::get('/{id}', 'App\Http\Controllers\Api\VehicleController@show');
+        Route::post('/', 'App\Http\Controllers\Api\VehicleController@store');
+        Route::put('/{id}', 'App\Http\Controllers\Api\VehicleController@update');
+        Route::delete('/{id}', 'App\Http\Controllers\Api\VehicleController@destroy');
+    });
+});
